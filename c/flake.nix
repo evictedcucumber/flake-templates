f0d338da7f4fdd7954f93025911254bed3902743
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2025-04-25 21:40:52 +0000

    feat(c): Added c flake.nix

---
+++ c/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A default C flake.";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell.override {stdenv = pkgs.clang19Stdenv;} {
+      name = "";
+      packages = with pkgs; [gnumake clang-tools];
+    };
+  };
+}
